1eccb71f480022302337b9c84b1a54c462b40ef6
lucasdiego33 <lukinha_candido@hotmail.com>
2024-04-10 19:24:58 -0300

    landing-pag

---
+++ script.js
@@ -0,0 +1,18 @@
+ 
+const p = document.querySelector("p")
+
+
+
+function btn(){
+      p.innerHTML = "A primeira sede localizava-se no Largo do Ouvidor, bem próximo à Faculdade de Direito de São Paulo, sob o nome comercial de Livraria Acadêmica. Expandiu suas lojas em mais de 18 Unidades da Federação do Brasil (nas 4 praças do SE; nas 3 praças do S; em 3 praças do CO - DF, GO, MS; em seis estados do NE - BA, CE, PB, PE, RN, SE e em apenas dois estados do N - AM, PA), sendo encontrada principalmente em shoppings."
+      
+}
+
+
+
+
+
+
+
+
+
